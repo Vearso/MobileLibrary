b0d58630ad9fa89fcb69a97331a0eb3464343769
Vearso <gus.szymon@gmail.com>
2020-10-27 11:43:48 +0100

    TypeScript config

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Header.scss';
+
+const Header = () => {
+    return(
+      <header>
+        <h1>Hello World!</h1>
+      </header>
+    )
+}
+
+export default Header;
