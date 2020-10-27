15d74f2886ba99ddd24de44d12c11906c2c8da5b
Vearso <gus.szymon@gmail.com>
2020-10-27 10:51:21 +0100

    Basic configuration - sass + catalogues structure created

---
+++ src/layouts/App/App.js
@@ -0,0 +1,14 @@
+import './App.scss';
+import React from "react";
+import Header from "../../components/Header/Header";
+
+
+function App() {
+  return (
+    <div className="App">
+        <Header/>
+    </div>
+  );
+}
+
+export default App;
