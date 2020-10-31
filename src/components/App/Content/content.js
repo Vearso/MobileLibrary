436b1aca11d94ebbea71ebbe4024ae37367da428
Vearso <gus.szymon@gmail.com>
2020-10-31 14:49:58 +0100

    Link to specified book - generating this book

--- src/components/App/Content/content.js
+++ src/components/App/Content/content.js
@@ -1,14 +1,19 @@
 import React, {useState, useEffect} from 'react';
 import {getVolumes} from "../../Books/API/requests";
 import {POSTER} from "../../Books/API/paths";
+import SearchedBook from "../../Books/Book/SearchedBook";
+import './content.scss';
 
-const Content = ({books}) => {
-
-
-    return(
-        <h1>
-            {books.length !== 0 ? books.items.map(item => <img src={`${item.volumeInfo.imageLinks.smallThumbnail}`}/>) : 'ehh'}
-        </h1>
+const Content = ({books,getBookID}) => {
+    return (
+        <div className='page__content'>
+            {books.length !== 0 ? books.map(item => <SearchedBook key={item.id}
+                                                                  id={item.id}
+                                                                  title={item.title}
+                                                                  author={item.authors}
+                                                                  onClick={getBookID}
+                                                                  img={item.imageLinks ? item.imageLinks.smallThumbnail : 'bannerS1'}/>) : 'Loading...'}
+        </div>
     )
 }
 export default Content;
