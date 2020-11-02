91b117faa716059f575d2d705817634e32d9c99f
Vearso <gus.szymon@gmail.com>
2020-11-02 09:18:04 +0100

    Fixing errors for netlify

--- src/components/MobileLibrary/Books/Book/SearchedBooks/SearchedBook.js
+++ src/components/MobileLibrary/Books/Book/SearchedBooks/SearchedBook.js
@@ -10,7 +10,7 @@ const SearchedBook = ({id, title, author, img , onClick}) => {
     return (
         <div className="searched__book" >
             <Link to={`${MOBILE_LIBRARY}/books/${id}`}>
-                <img className="book--poster" src={img} onClick={handleClick}/>
+                <img className="book--poster" src={img} alt={title} onClick={handleClick}/>
             </Link>
             <p className="book--title">{title ? title : "Title not found"}</p>
             <p className="book--author">by {author ? author.join(' ') : "Author not found"}</p>
