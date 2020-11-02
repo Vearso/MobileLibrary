2e82ab8267db586e5f46eb11ce8e6fc6cf614c19
Vearso <gus.szymon@gmail.com>
2020-11-02 19:21:22 +0100

    Netlify error fixed

--- src/components/MobileLibrary/Content/MainPage/MainPage.js
+++ src/components/MobileLibrary/Content/MainPage/MainPage.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import SearchedBook from "../../Books/Book/SearchedBooks/SearchedBook";
-import './MainPage.scss';
-
-const MainPage = () => {
-    return (
-        null
-    )
-}
-export default MainPage;
+// import React from 'react';
+// import SearchedBook from "../../Books/Book/SearchedBooks/SearchedBook";
+// import './MainPage.scss';
+//
+// const MainPage = () => {
+//     return (
+//         null
+//     )
+// }
+// export default MainPage;
