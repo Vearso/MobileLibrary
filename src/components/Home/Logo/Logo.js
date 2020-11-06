25992f74dc41b141280d39a785db5af930a70203
Vearso <gus.szymon@gmail.com>
2020-11-06 14:17:20 +0100

    Last repairs

--- src/components/Home/Logo/Logo.js
+++ src/components/Home/Logo/Logo.js
@@ -5,7 +5,7 @@ const Logo = ({show}) => {
     const width = window.innerWidth;
         return (
             <div className='logo'>
-                <img className='logo__img' src='logo192.png' alt='Logo of the page'/>
+                <img className='logo__img' src='/logo192.png' alt='Logo of the page'/>
                 <h2 className={width < 450 ? show ? 'disabled' : 'logo__title' : 'logo__title'}>Mobile<span>Library</span></h2>
             </div>
         )
